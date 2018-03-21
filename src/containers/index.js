34c1970e2b381e02cd3242a2c05f2721d82a433d
Andy Tsai <bbandydd@gmail.com>
2018-03-21 11:31:28 +0800

    add react-loadable and react-loading

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 // import package
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
 // import relative path
-import Home from './Home';
+import Loading from './Loading';
 import NotFound from './NotFound';
+
+const LoadComponent = loader => Loadable({
+  loader,
+  loading: Loading,
+})
+
+const Home = LoadComponent(() => import('./Home'));
 
 const Main = () => (
   <Router>
