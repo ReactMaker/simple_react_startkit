0553927ea684665cf3c645440f7f3e3fa7ba69c2
chnbohwr <chnbohwr@gmail.com>
2018-09-02 15:29:50 +0800

    fix class option

--- src/containers/index.js
+++ src/containers/index.js
@@ -5,7 +5,7 @@ export default class Main extends Component {
     return (
       <div style={{ textAlign: 'center' }}>
         <h2>Hello, I`m ReactMaker.</h2>
-        <p>To get started, edit containers/index.js wer</p>
+        <p>To get started, edit containers/index.js</p>
       </div>
     );
   }
