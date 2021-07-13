cc4b80584ef66969c68e1d4b0ac98b71166f8687
hyman_chen <hyman_chen@xxentria-tech.com>
2021-07-13 02:42:29 +0000

    update readme

--- src/containers/index.js
+++ src/containers/index.js
@@ -8,7 +8,6 @@ const Main = () => {
     <div style={{ textAlign: 'center' }}>
       <h2>Hello, I`m ReactMaker.</h2>
       <p>To get started, edit containers/index.js</p>
-      <div>asdf</div>
     </div>
   );
 };
